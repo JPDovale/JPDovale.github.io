5ee95d3ecd4a5f4d3a23e4e9fa92faa64bc536ed
JPDovale <joaopaulodovaledeo.60@gmail.com>
2022-02-27 18:03:25 -0300

    feed

--- projeto/scripts/interface.js
+++ projeto/scripts/interface.js
@@ -2,35 +2,43 @@ let BODY = document.getElementById("body")
 
 function openWindowFilterServise(){
     let windowFilterServise = document.getElementById("windowFilterService")
-    
-    if (windowFilterServise.style.display === 'flex'){
-        windowFilterServise.style.display = 'none'
-        BODY.style.display = 'none'
-    } else{
+    let feedNone = document.getElementById('feed')
+    let bar = document.getElementById('searchBarr')
+let windowProfileOpitions = document.getElementById("windowProfileOptions")
+    if (windowFilterServise.style.display != 'flex'){
         BODY.style.display = 'flex'
         windowFilterServise.style.display = 'flex'
-    }
-}
-
-
-function openWindowProfileOpitions() {
-    let windowProfileOpitions = document.getElementById("windowProfileOptions")
-    let img = document.getElementById('imgPesq')
-
-    if(windowProfileOpitions.style.display === 'block'){
-        windowProfileOpitions.style.display = 'none'
-        BODY.style.display = 'none'
-        img.style.display = 'flex'
-    }else{
-        BODY.style.display = 'flex'
+        feedNone.style.display ='none'
+        bar.style.display = 'none'
         windowProfileOpitions.style.display = 'block'
-        img.style.display = 'none'
+        
+    } else{
+        windowFilterServise.style.display = 'none'
+        BODY.style.display = 'none'
+        feedNone.style.display ='block'
+        bar.style.display = 'flex'
+         windowProfileOpitions.style.display = 'none'
     }
 }
+
+// function openWindowProfileOpitions() {
+    
+//     let img = document.getElementById('imgPesq')
+
+//     if(windowProfileOpitions.style.display === 'block'){
+       
+//         BODY.style.display = 'none'
+//         img.style.display = 'flex'
+//     }else{
+//         BODY.style.display = 'flex'
+        
+//         img.style.display = 'none'
+//     }
+// }
 
 function viewEspeciflyFilter(){
     let boxFilter = document.getElementById("boxFilter")
-
+    
     if(boxFilter.style.display === 'flex'){
         boxFilter.style.display = 'none'
         
