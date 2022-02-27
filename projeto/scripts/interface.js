7dc15c32b67ef1fdf0cbf12ebb65d2831d239ee7
JPDovale <joaopaulodovaledeo.60@gmail.com>
2022-02-27 02:30:01 -0300

    Aba de busca
    atualizando

--- projeto/scripts/interface.js
+++ projeto/scripts/interface.js
@@ -1,9 +1,13 @@
+let BODY = document.getElementById("body")
+
 function openWindowFilterServise(){
     let windowFilterServise = document.getElementById("windowFilterService")
     
     if (windowFilterServise.style.display === 'flex'){
         windowFilterServise.style.display = 'none'
+        BODY.style.display = 'none'
     } else{
+        BODY.style.display = 'flex'
         windowFilterServise.style.display = 'flex'
     }
 }
@@ -11,10 +15,27 @@ function openWindowFilterServise(){
 
 function openWindowProfileOpitions() {
     let windowProfileOpitions = document.getElementById("windowProfileOptions")
+    let img = document.getElementById('imgPesq')
 
     if(windowProfileOpitions.style.display === 'block'){
         windowProfileOpitions.style.display = 'none'
+        BODY.style.display = 'none'
+        img.style.display = 'flex'
     }else{
-         windowProfileOpitions.style.display = 'block'
+        BODY.style.display = 'flex'
+        windowProfileOpitions.style.display = 'block'
+        img.style.display = 'none'
+    }
+}
+
+function viewEspeciflyFilter(){
+    let boxFilter = document.getElementById("boxFilter")
+
+    if(boxFilter.style.display === 'flex'){
+        boxFilter.style.display = 'none'
+        
+    }else{
+        boxFilter.style.display = 'flex'
+        
     }
 }
