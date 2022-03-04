bffba4bb2ac2f84769e971be9ad7c401969c50a7
JPDovale <joaopaulodovaledeo.60@gmail.com>
2022-03-04 01:06:57 -0300

    shelpage

---
+++ portifolio/script/DMDPage.js
@@ -0,0 +1,15 @@
+ // Your web app's Firebase configuration
+        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+        const firebaseConfig = {
+            apiKey: "AIzaSyBPXSsZniGo_zyKa02Ao3N_GZQ_XTfvH6w",
+            authDomain: "depois-da-meia-noite.firebaseapp.com",
+            projectId: "depois-da-meia-noite",
+            storageBucket: "depois-da-meia-noite.appspot.com",
+            messagingSenderId: "550167799371",
+            appId: "1:550167799371:web:5f1331c89be5b1abd70fce",
+            measurementId: "G-Q8SD96C0XP"
+          };
+        
+          // Initialize Firebase
+          const app = initializeApp(firebaseConfig);
+          const analytics = getAnalytics(app);
