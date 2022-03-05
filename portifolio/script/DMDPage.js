9633a90459699da6ea7a8ccc97ad86895036e9a7
JPDovale <joaopaulodovaledeo.60@gmail.com>
2022-03-04 23:41:58 -0300

    PAGINA DE VENDA

--- portifolio/script/DMDPage.js
+++ portifolio/script/DMDPage.js
@@ -1,52 +1,28 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBPXSsZniGo_zyKa02Ao3N_GZQ_XTfvH6w",
-  authDomain: "depois-da-meia-noite.firebaseapp.com",
-  projectId: "depois-da-meia-noite",
-  storageBucket: "depois-da-meia-noite.appspot.com",
-  messagingSenderId: "550167799371",
-  appId: "1:550167799371:web:4db52be045b93390d70fce",
-  measurementId: "G-ZT6ZBTDM4V"
-};
+let NAME11 = document.getElementById("name")
+let EMAIL = document.getElementById("email")
+let tel = document.getElementById("tel")
+let streat = document.getElementById("streat")
+let complement = document.getElementById("complement")
+let numberHouse = document.getElementById("number")
+let CEP = document.getElementById("cep")
+let district = document.getElementById("district")
+let city = document.getElementById("city")
+let state = document.getElementById("state")
+const DB = firebase.firestore()
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-const NAME11 = document.getElementById("name")
-const email = document.getElementById("email")
-const tel = document.getElementById("tel")
-const streat = document.getElementById("streat")
-const complement = document.getElementById("complement")
-const numberHouse = document.getElementById("number")
-const cep = document.getElementById("cep")
-const district = document.getElementById("district")
-const city = document.getElementById("city")
-const state = document.getElementById("state")
-const finish = document.getElementById("finish") 
-const VENDA = "venda"
-let db = firebase.firestore()
-
-
-finish.addEventListner('click', finish1() )
-function finish1(){
-    db.collection(VENDA).add({
+function finishShop2(){
+    DB.collection("venda").add({
     nome: NAME11.value,
-    email: email.value,
+    email: EMAIL.value,
     telefone: tel.value,
     endereço: streat.value,
     complemento: complement.value,
     numero: numberHouse.value,
-    cep: cep.value,
+    cep: CEP.value,
     bairo: district.value,
     cidade: city.value,
     estado: state.value,
-  })
+  },{marge: true})
+ 
 }
